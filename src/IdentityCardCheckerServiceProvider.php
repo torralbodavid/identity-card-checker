f077a0a53a8cceed8f8907d2ab6b9ce4e7adef04
David Torralbo <davidtorralboperez@gmail.com>
2021-11-07 22:29:33 +0100

    Update service provider

--- src/IdentityCardCheckerServiceProvider.php
+++ src/IdentityCardCheckerServiceProvider.php
@@ -4,22 +4,14 @@ namespace Torralbodavid\IdentityCardChecker;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Torralbodavid\IdentityCardChecker\Commands\IdentityCardCheckerCommand;
 
 class IdentityCardCheckerServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('identity-card-checker')
             ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_identity-card-checker_table')
-            ->hasCommand(IdentityCardCheckerCommand::class);
+            ->hasViews();
     }
 }
